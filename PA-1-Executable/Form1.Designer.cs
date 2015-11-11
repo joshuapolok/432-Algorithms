c1e546c4e5bcd89fb6e6e683307b7336602daf9c
Joshua Poling <joshuapolok@hotmail.com>
2015-11-11 14:43:54 -0600

    finished merge sort

--- PA-1-Executable/Form1.Designer.cs
+++ PA-1-Executable/Form1.Designer.cs
@@ -35,11 +35,13 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.inputStream = new System.Windows.Forms.TextBox();
+            this.mergeLabel = new System.Windows.Forms.Label();
+            this.quickLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // submit
             // 
-            this.submit.Location = new System.Drawing.Point(43, 40);
+            this.submit.Location = new System.Drawing.Point(43, 28);
             this.submit.Name = "submit";
             this.submit.Size = new System.Drawing.Size(54, 23);
             this.submit.TabIndex = 0;
@@ -50,24 +52,24 @@
             // mergeBox
             // 
             this.mergeBox.FormattingEnabled = true;
-            this.mergeBox.Location = new System.Drawing.Point(12, 108);
+            this.mergeBox.Location = new System.Drawing.Point(86, 96);
             this.mergeBox.Name = "mergeBox";
-            this.mergeBox.Size = new System.Drawing.Size(258, 264);
+            this.mergeBox.Size = new System.Drawing.Size(132, 199);
             this.mergeBox.TabIndex = 2;
             // 
             // quickBox
             // 
             this.quickBox.FormattingEnabled = true;
-            this.quickBox.Location = new System.Drawing.Point(299, 109);
+            this.quickBox.Location = new System.Drawing.Point(348, 97);
             this.quickBox.Name = "quickBox";
-            this.quickBox.Size = new System.Drawing.Size(258, 264);
+            this.quickBox.Size = new System.Drawing.Size(132, 199);
             this.quickBox.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(23, 20);
+            this.label1.Location = new System.Drawing.Point(23, 8);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(247, 17);
             this.label1.TabIndex = 4;
@@ -77,7 +79,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(24, 76);
+            this.label2.Location = new System.Drawing.Point(24, 64);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(236, 29);
             this.label2.TabIndex = 5;
@@ -87,7 +89,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(303, 76);
+            this.label3.Location = new System.Drawing.Point(303, 64);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(230, 29);
             this.label3.TabIndex = 6;
@@ -95,16 +97,36 @@
             // 
             // inputStream
             // 
-            this.inputStream.Location = new System.Drawing.Point(103, 43);
+            this.inputStream.Location = new System.Drawing.Point(103, 31);
             this.inputStream.Name = "inputStream";
             this.inputStream.Size = new System.Drawing.Size(454, 20);
             this.inputStream.TabIndex = 1;
+            // 
+            // mergeLabel
+            // 
+            this.mergeLabel.AutoSize = true;
+            this.mergeLabel.Location = new System.Drawing.Point(9, 308);
+            this.mergeLabel.Name = "mergeLabel";
+            this.mergeLabel.Size = new System.Drawing.Size(91, 13);
+            this.mergeLabel.TabIndex = 7;
+            this.mergeLabel.Text = "Merge Sort Time: ";
+            // 
+            // quickLabel
+            // 
+            this.quickLabel.AutoSize = true;
+            this.quickLabel.Location = new System.Drawing.Point(296, 308);
+            this.quickLabel.Name = "quickLabel";
+            this.quickLabel.Size = new System.Drawing.Size(89, 13);
+            this.quickLabel.TabIndex = 8;
+            this.quickLabel.Text = "Quick Sort Time: ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(571, 385);
+            this.ClientSize = new System.Drawing.Size(571, 339);
+            this.Controls.Add(this.quickLabel);
+            this.Controls.Add(this.mergeLabel);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -129,6 +151,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox inputStream;
+        private System.Windows.Forms.Label mergeLabel;
+        private System.Windows.Forms.Label quickLabel;
     }
 }
 
--- PA-1-Executable/Form1.cs
+++ PA-1-Executable/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Timers;
 using System.Windows.Forms;
 
@@ -14,9 +15,11 @@ namespace PA_1_Executable
 {
     public partial class Form1 : Form
     {
-
-        private static System.Timers.Timer aTimer;
-        int time = 1;
+        // timers and stopwatches for sorting times
+        public Stopwatch watch1 = new Stopwatch();
+        public Stopwatch watch2 = new Stopwatch();
+        public TimeSpan span1;
+        public TimeSpan span2;
 
         public Form1()
         {
@@ -24,7 +27,10 @@ namespace PA_1_Executable
         }
 
         /// <summary>
-        /// Split the inputed items and display them in the listbox area
+        /// Split the inputed items and display them in the listbox area. Start
+        /// a timer for each sorting function and end the timer after the list
+        /// has been sorted. Once the lists are sorted display the time it 
+        /// took for the sort to complete.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -38,6 +44,9 @@ namespace PA_1_Executable
         /// </history>
         private void SubmitButton(object sender, EventArgs e)
         {
+            quickBox.Items.Clear();
+            mergeBox.Items.Clear();
+
             var text = inputStream.Text;
             var list = new List<string>();
 
@@ -52,11 +61,19 @@ namespace PA_1_Executable
                 quickBox.Items.Add(item);
             }
 
-          
-
+            watch1.Start();
             QuickSort(parsedList, 0, parsedList.Count - 1);
+            watch1.Stop();
 
-           
+            span1 = watch1.Elapsed;
+            quickLabel.Text = "Quick Sort Time: " + span1;
+
+            watch2.Start();
+            MergeSort(parsedList, 0, parsedList.Count - 1);
+            watch2.Stop();
+
+            span2 = watch2.Elapsed;
+            mergeLabel.Text = "Merge Sort Time: " + span2;
 
         }
 
@@ -73,12 +90,80 @@ namespace PA_1_Executable
         /// 
         /// ----------------------------------------------------------------
         /// </history>
-        public void MergeSort()
+        public void MergeSort(List<int> numbers, int left, int right)
         {
-           
-        }
+            int mid;
 
-        public  void QuickSort(List<int> elements,int left, int right)
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                MergeSort(numbers, left, mid);
+                MergeSort(numbers, (mid + 1), right);
+
+                Merge(numbers, left, (mid + 1), right);
+            }
+        }
+    
+        /// <summary>
+        /// Merge sort the items in the listbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <author>Joshua Poling, Erick Roberson, Thanh Nguyen</author>
+        /// <date>November 11, 2015</date>
+        /// <history>
+        /// Date   Author    Change Reason     Change Description
+        /// ----------------------------------------------------------------
+        /// 
+        /// ----------------------------------------------------------------
+        /// </history>
+        public void Merge(List<int> numbers, int left, int mid, int right)
+        {
+            int[] temp = new int[numbers.Count];
+            int i, left_end, num_elements, tmp_pos;
+
+            left_end = (mid - 1);
+            tmp_pos = left;
+            num_elements = (right - left + 1);
+
+            while ((left <= left_end) && (mid <= right))
+            {
+                if (numbers[left] <= numbers[mid])
+                    temp[tmp_pos++] = numbers[left++];
+                else
+                    temp[tmp_pos++] = numbers[mid++];
+            }
+
+            while (left <= left_end)
+                temp[tmp_pos++] = numbers[left++];
+
+            while (mid <= right)
+                temp[tmp_pos++] = numbers[mid++];
+
+            for (i = 0; i < num_elements; i++)
+            {
+                numbers[right] = temp[right];
+                mergeBox.Items.RemoveAt(right);
+                mergeBox.Items.Insert(right,numbers[i]);
+                right--;
+            }
+        }
+    
+
+        /// <summary>
+        /// Merge sort the items in the listbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <author>Joshua Poling, Erick Roberson, Thanh Nguyen</author>
+        /// <date>November 11, 2015</date>
+        /// <history>
+        /// Date   Author    Change Reason     Change Description
+        /// ----------------------------------------------------------------
+        /// 
+        /// ----------------------------------------------------------------
+        /// </history>
+        public void QuickSort(List<int> elements,int left, int right)
         {
             int i = left;
             int j = right;
@@ -94,18 +179,16 @@ namespace PA_1_Executable
                 {
                     j--;
                 }
-
-
+                
                 if (i <= j)
                 {
-                    // Swap
+                    // Swap, and swap elements in text box
                     int tmp = elements[i];
                     elements[i] = elements[j];
-                    elements[j] = tmp;
-
-                    
                     quickBox.Items.RemoveAt(i);
                     quickBox.Items.Insert(i, elements[j]);
+
+                    elements[j] = tmp;
                     quickBox.Items.RemoveAt(j);
                     quickBox.Items.Insert(j, tmp);
                     
@@ -123,23 +206,5 @@ namespace PA_1_Executable
                 QuickSort(elements, i, right);
             }
         }
-
-        private static void SetTimer()
-        {
-            // Create a timer with a two second interval.
-            aTimer = new System.Timers.Timer(2000);
-
-            // Hook up the Elapsed event for the timer. 
-            aTimer.Elapsed += OnTimedEvent;
-            aTimer.AutoReset = true;
-            aTimer.Enabled = true;
-        }
-
-        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
-        {
-            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
-                              e.SignalTime);
-        }
-
     }
 }
